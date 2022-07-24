4be1b9f9064f1623b4a5234ecd04af960ec3df4e
amir-android-dev <amir-hosseini@gmx.at>
2022-07-24 22:02:59 +0200

    field injection dagger

--- app/src/main/java/com/amir/dependencydemo/MainActivity.kt
+++ app/src/main/java/com/amir/dependencydemo/MainActivity.kt
@@ -2,32 +2,31 @@ package com.amir.dependencydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Inject
+
 /*
-after dagger and dependencies we have to rebuild our project
- */
-/*
-You can see dagger has created factory classes for each dependency.
- And for the component interface dagger has created a class named DaggerSmartPhoneComponnent implementing the interface.
-  When generating a class which implements component interface, dagger always include the word Dagger in front of the interface name.
+Why we did this new change.
+
+If we invoke a function like getSmartPhone. We will be able to only get that SmartPhone dependency.
+If we wanted get another dependency we would have to create another function in the component interface and invoked it here.
+But, now we don’t need to create a specific function for each and every dependency. Instead we can get those dependencies to the MainActivity using field injection.
+ as an example if we want ot inject the sim card the only thing that we need is doing this
+ @Inject
+    lateinit var simCard: SIMCard
  */
 class MainActivity : AppCompatActivity() {
-    private lateinit var smartPhone: SmartPhone
+    @Inject
+    lateinit var smartPhone: SmartPhone
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-          DaggerSmartPhoneComponent.create()
-              .getSmartPhone()
-              .makeACallWithRecording()
-//        val battery = Battery()
-//        val memoryCard = MemoryCard()
-//        val serviceProvider = ServiceProvider()
-//        val simCard = SIMCard(serviceProvider())
+        DaggerSmartPhoneComponent.create()
+            .inject(this)
+        smartPhone.makeACallWithRecording()
 
-        // val smartPhone=SmartPhone(battery,simCard, memoryCard)
-
-//        val smartPhone = SmartPhone(Battery(), SIMCard(ServiceProvider()), MemoryCard())
-//        .makeACallWithRecording()
 
     }
 }
--- app/src/main/java/com/amir/dependencydemo/SmartPhoneComponent.kt
+++ app/src/main/java/com/amir/dependencydemo/SmartPhoneComponent.kt
@@ -2,16 +2,26 @@ package com.amir.dependencydemo
 
 import dagger.Component
 
-/*to use dagger 2 generated codes for dependency injection
-we also need an interface annotated with @Component annotation.  */
+/*
+In our demo project, to get the SmartPhone dependency,
+ we have invoked this getSmartPhone function of the smartPhoneComponent interface form the MainActivity,
+ But this is not the best and most efficient way of doing it.
+ There can be many activities and fragments in a project.
+ If you have 10 required dependencies like this SmartPhone dependency, you may have to write getter methods for all of them
+ in the component interface. And you will have to call to them form all activities in this way.
+ */
 
 @Component(modules = [MemoryCardModule::class, NCBatteryModule::class])
 interface SmartPhoneComponent {
-    //an abstract function to get the dependency we want
-    //the name of function is not important
-    //the return type function must be the type of the dependency we want
-    fun getSmartPhone():SmartPhone
-//we have to link our module to this component
+   //we dont have to write getter method like this
+    //we can get injected all dependencies belong to the dependency graph
+  //  fun getSmartPhone():SmartPhone
+    //we can use every name for it but as a convention we make use of inject
+    /*
+    If in case you need to use this component in 3 activities and one fragment,
+     you may need to write 3 more inject functions here with those activities and fragment as parameters.
+     */
+    fun inject(mainActivity: MainActivity)
 
 
 }
